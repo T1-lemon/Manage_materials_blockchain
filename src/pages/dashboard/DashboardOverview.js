d69c049c8432e17c8e04ad3381e852ee0cb3f2e4
T1-lemon <tuandeptrai2111@gmail.com>
2022-12-29 21:57:27 +0700

    update:v1

--- src/pages/dashboard/DashboardOverview.js
+++ src/pages/dashboard/DashboardOverview.js
@@ -34,12 +34,50 @@ export default () => {
   };
 
   return (
-    <div className="dashboard_container">
-      <img
-        src="https://4.bp.blogspot.com/-r9ym602WfE4/Wjh160vYw8I/AAAAAAAAB2w/kpNHAcY46FASzoZm8U6-QyENwD6jyuyGACLcBGAs/s1600/AAEAAQAAAAAAAAVFAAAAJDY3OTZhNGVlLTBjZGQtNDUwNC1iNTkxLWRmNWIzZWMwNDJhNQ.png"
-        alt=""
-        className="etherum"
-      />
-    </div>
+    <>
+      <Row className="mt-2">
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Thành viên nhóm thực hiện </Card.Title>
+              <Card.Text>Bùi Anh Tuấn</Card.Text>
+              <Card.Text>Đỗ Văn Đức Thành</Card.Text>
+              <Card.Text>Nguyễn Thị Hương</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Quy trình nhập sản phẩm</Card.Title>
+              <Card.Text>
+                Bao gồm các bước thực hiện để nhập một sản phẩm vào blockchain
+              </Card.Text>
+              <Button variant="primary" className="w-100">Link</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Code repository</Card.Title>
+              <Card.Text>Code repository bao gồm Frontend, Backend</Card.Text>
+              <Button variant="primary" className="w-100">Link</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6}>
+        <Card>
+            <Card.Body>
+              <Card.Title>Get Started</Card.Title>
+              <Card.Text>Bắt đầu trải nghiệm sản phẩm</Card.Text>
+              <Button variant="primary" className="w-100">Link</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
